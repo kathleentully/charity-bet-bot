1141902756ba498dd0413a84aac2cadbc3ba5b5d
Kathleen Mege <>
2020-11-01 17:01:59 -0500

    split file a bit

---
+++ UserState.py
@@ -0,0 +1,31 @@
+from collections import namedtuple
+from math import floor
+
+Deal = namedtuple('Deal', 'price tickets')
+#should be ordered high to low, too lazy to enforce
+EVENT_PRICES = [
+    Deal(price=20, tickets=25),
+    Deal(price=10, tickets=11),
+    Deal(price=1, tickets=1)
+]
+
+class UserState:
+    def __init__(self):
+        self.tickets_available = 0
+        self.amount_owed = 0
+        self.bets = []
+
+    def __str__(self):
+        return f'Tickets available: {self.tickets_available}, Amount owed: ${self.amount_owed}'
+
+    def buyin(self, money_amount):
+        charge = 0
+        tickets = 0
+        for deal in EVENT_PRICES:
+            number_of_sets = floor(money_amount / deal.price)
+            charge += number_of_sets * deal.price
+            money_amount -= number_of_sets * deal.price
+            tickets += number_of_sets * deal.tickets
+
+        self.amount_owed += charge
+        self.tickets_available += tickets
--- bot.py
+++ bot.py
@@ -1,4 +1,3 @@
-from collections import namedtuple
 from dotenv import load_dotenv
 import discord
 from discord.ext.commands import Bot
@@ -7,47 +6,21 @@ from os import getenv
 from random import choice
 import traceback 
 
+from UserState import UserState, EVENT_PRICES
+
 load_dotenv()
 
 ADMIN_USERS = [int(x) for x in getenv("ADMIN_USERS").split(",")]
 COMMAND_PREFIX = '!'
 game_state = {}
-Deal = namedtuple('Deal', 'price tickets')
-#should be ordered high to low, too lazy to enforce
-EVENT_PRICES = [
-    Deal(price=20, tickets=25),
-    Deal(price=10, tickets=11),
-    Deal(price=1, tickets=1)
-]
-VENMO_USERNAME_FOR_DONATIONS = f'@{getenv('VENMO_USERNAME')}'
+
+VENMO_USERNAME_FOR_DONATIONS = f'@{getenv("VENMO_USERNAME")}'
 open_bets = {}
 bet_id_cursor = 0
 used_bet_ids = set()
 bet_id_semaphore = True # green light
 log_channel = None
 bot = Bot(command_prefix=COMMAND_PREFIX)
-
-
-class UserState:
-    def __init__(self):
-        self.tickets_available = 0
-        self.amount_owed = 0
-        self.bets = []
-
-    def __str__(self):
-        return f'Tickets available: {self.tickets_available}, Amount owed: ${self.amount_owed}'
-
-    def buyin(self, money_amount):
-        charge = 0
-        tickets = 0
-        for deal in EVENT_PRICES:
-            number_of_sets = floor(money_amount / deal.price)
-            charge += number_of_sets * deal.price
-            money_amount -= number_of_sets * deal.price
-            tickets += number_of_sets * deal.tickets
-
-        self.amount_owed += charge
-        self.tickets_available += tickets
 
 
 async def log(msg):
